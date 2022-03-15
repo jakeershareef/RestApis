24ca70c0c875183d125def8a02976e9dbdd6736d
jakeershareef <36979961+jakeershareef@users.noreply.github.com>
2022-03-15 12:08:18 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
+const userRoute = require('./routes/data');
+const app = express();
+dotenv.config();
+app.use(bodyParser.json());
+
+mongoose.connect(process.env.MONGO_URI).then(() => {
+    console.log('DB is connected')
+}).catch((err) => {
+    console.log('DB is not connected')
+});
+app.use('/',userRoute)
+const port = 5000;
+app.listen(port, () => {
+    console.log(`app is running on ${port}`)
+})
